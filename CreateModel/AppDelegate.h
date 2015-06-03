b60437155bcd71a91db21e1326e990ae7f1a5565
YouXianMing <705786299@qq.com>
2015-06-04 03:46:48 +0800

    Initial Commit

---
+++ CreateModel/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CreateModel
+//
+//  Created by YouXianMing on 15/6/4.
+//  Copyright (c) 2015年 YouXianMing. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CreateModel/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  CreateModel
+//
+//  Created by YouXianMing on 15/6/4.
+//  Copyright (c) 2015年 YouXianMing. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
